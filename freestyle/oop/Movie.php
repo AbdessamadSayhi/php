5a9cf2ef37c9a4a5b7e0f5da1522d5efa619728b
Abdessamad <abdessamadsayhi17@gmail.com>
2024-02-23 17:48:14 +0100

    Learn PHP in One Day and Learn It Well - continue practicing

--- freestyle/oop/Movie.php
+++ freestyle/oop/Movie.php
@@ -16,4 +16,38 @@ class Movie
         $this->title = $pTitle;
         $this->rentalPrice = $pRentalPrice;
     }
+
+    public function conversion($country)
+    {
+        $rate = 1;
+        switch ($country) {
+            case 'MA':
+                $rate = 10.06;
+                break;
+            case 'USA':
+                $rate = 1;
+                break;
+        }
+        return round($rate * $this->rentalPrice, 2);
+    }
+
+    public function __get($propertyRequested)
+    {
+        if ($propertyRequested == 'id')
+            return 'You do not have permission to access
+        id.<BR>';
+        else
+            return $this->$propertyRequested;
+    }
+    public function __set($propertyToModify, $value)
+    {
+        if (
+            $propertyToModify == 'rentalPrice' && $value
+            > $this->rentalPrice
+        )
+            $this->rentalPrice = $value;
+        else
+            echo 'Failed to modify
+        ' . $propertyToModify . '<BR>';
+    }
 }
